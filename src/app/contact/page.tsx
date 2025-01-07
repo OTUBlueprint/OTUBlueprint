408c9c3d314d2a4866bd8600b5c5cb7f1b1f2d48
ClaytonWas <claywasmund@gmail.com>
2025-01-07 10:15:37 -0500

    Contact page revised. Just needs image and a check over to match stylings.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,26 +1,33 @@
 import Link from 'next/link';
+import EmailIcon from "@/logos/email-icon.svg"
+import InstagramLogo from "@/logos/instagram-logo.svg"
+import DiscordLogo from "@/logos/discord-logo.svg"
 
 const Contact = () => (
-  <div className='max-w-prose flex flex-col m-4 gap-4 md:gap-8 md:justify-center'>
-    <p className="text-2xl md:text-6xl">Contact us!</p>
-    <p className="text-lg md:text-2xl">Have a question? Interested in a potential project but unsure what our team can help you with? Send us an email or connect with us on our social media!</p>
-    <div className="flex flex-row gap-2">
-      <Link href="https://www.google.com">
-          <p>svg here</p>
+  <div className='md:flex items-center justify-center min-h-[calc(80vh)] p-4'>
+    <div className='max-w-prose flex flex-col m-4 gap-4 md:gap-8 md:justify-center'>
+      <p className="font-semibold text-2xl md:text-4xl">Get in Touch!</p>
+      <p className="text-lg md:text-2xl md:ml-6">
+        Have a question? Curious about the club, a potential project, 
+        or just want to connect with a Blueprint executive?
+        Don't hesitate to reach out to us!
+      </p>
+      <Link href="mailto:connect@otublueprint.org?subject=Website%20Redirect:" className='flex flex-row gap-2 items-center group md:ml-6'>
+        <EmailIcon className="fill-white md:h-12 md:w-12 group-hover:fill-indigo-200"/>
+        <p className="text-2xl md:text-3xl group-hover:text-indigo-200">otublueprint@hotmail.com</p>
       </Link>
-      <Link href="https://www.google.com">
-          <p>svg here</p>
-      </Link>
-      <Link href="https://www.google.com">
-          <p>svg here</p>
-      </Link>
-      <Link href="https://www.google.com">
-          <p>svg here</p>
-      </Link>
+      <div className="flex flex-row gap-8 md:ml-6">
+        <Link href="https://www.instagram.com/otublueprint/">
+            <InstagramLogo className="fill-white w-16 h-16 hover:fill-indigo-200" />
+        </Link>
+        <Link href="https://discord.gg/yXATsYcFC8">
+            <DiscordLogo className="fill-white w-16 h-16 hover:fill-indigo-200" />
+        </Link>
+      </div>
     </div>
-    <Link href="mailto:connect@otublueprint.org?subject=Website%20Redirect:">
-      <p className="text-2xl">connect@otublueprint.org</p>
-    </Link>
+    <div>
+      <p>Image will go here.</p>
+    </div>
   </div>
 );
 
