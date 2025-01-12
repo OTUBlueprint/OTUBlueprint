df30f78bf4f91be094c994bc821f68360ee1904f
ClaytonWas <claywasmund@gmail.com>
2025-01-12 13:02:45 -0500

    Revised landing page font sizes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,49 +14,48 @@ export default function Home() {
                     <div className="w-auto h-auto">
                         <Logo className="fill-white object-cover" />
                     </div>
-                    <p className="text-white mb-3">tech for social good. build by students.</p>
-                    <Button background="white" colour="blue">Learn More</Button>
+                    <p className="text-white mb-3">tech for social good. built by students.</p>
+                    <Link href="/team" className="text-xl"><Button background="white" colour="blue">Meet our Team</Button></Link>
                 </div>
             </section>
             <section className="flex flex-row justify-center py-12">
                 <div className="max-w-screen-lg px-4">
                     <div className="flex flex-col items-center">
-                        <p className="text-blue text-5xl">Who We Are</p>
+                        <p className="text-blue text-4xl">Who We Are</p>
                         <div className="w-24 mt-2 border border-blue" />
                     </div>
-                    <p className="my-4 text-lg">
+                    <p className="my-4 text-lg text-center">
                         We are a group of students at Ontario Tech University that are dedicate to developing technology
                         based solutions for nonprofit organizations.
                     </p>
-                    <Link href="/team" className=""><Button background="blue" colour="white">Meet our Team</Button></Link>
                 </div>
             </section>
             <section className="flex flex-row justify-center py-12">
                 <div className="max-w-screen-lg">
                     <div className="flex flex-row">
-                        <div className="pt-3 px-5 border-r-2 border-blue">
+                        <div className="p-4 border-r border-blue">
                             <div className="flex flex-col items-center text-center">
-                                <p className="text-blue text-5xl">Interested In Our Services?</p>
+                                <p className="text-blue text-2xl">Interested In Our Services?</p>
                                 <div className="w-24 mt-2 border border-blue" />
                             </div>
-                            <p className="my-4 text-center">
+                            <p className="my-4 text-center text-lg">
                                 Our team of skilled product managers, project leaders, designers, and developers is here
                                 to help your organization grow with creative solutions.
                             </p>
                         </div>
-                        <div className="pt-3 px-5 border-l-2 border-blue">
+                        <div className="p-4 border-l border-blue">
                             <div className="flex flex-col items-center text-center">
-                                <p className="text-blue text-5xl">Want to help?</p>
+                                <p className="text-blue text-2xl">Want to help?</p>
                                 <div className="w-24 mt-2 border border-blue" />
                             </div>
-                            <p className="my-4 text-center">
+                            <p className="my-4 text-center text-lg">
                                 Ready to make an impact? Learn how to create professional products that drive real world
                                 change and jumpstart your career with UW Blueprint!
                             </p>
                         </div>
                     </div>
                     <div className="flex justify-center mt-6">
-                        <Link href="/contact" className=""><Button background="blue" colour="white">Contact</Button></Link>
+                        <Link href="/contact" className="text-xl"><Button background="blue" colour="white">Contact</Button></Link>
                     </div>
                 </div>
             </section>
